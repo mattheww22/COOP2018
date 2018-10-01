7f23183d186cee917b92c36881e285a71c1ce536
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-10-01 10:24:39 -0500

    These are inital versions of Unit 2

--- Chapter03/U03_Ex01_Vol-Surface.py
+++ Chapter03/U03_Ex01_Vol-Surface.py
@@ -18,8 +18,8 @@
 #
 # Algorith (pseudocode)
 # Get input of radius
-# Calculate volume using (4/3(pi)r2)
-# Calculate surface area (4(pi)r2)
+# Calculate volume using (4/3(π)r2)
+# Calculate surface area (4(π)r2)
 # Print calculated volume and SA
 #
 #
@@ -27,13 +27,13 @@
 def main():
 
 # Get input of radius
-Radius = int(input("Enter Radius"))
+    Radius = int(input("Enter Radius"))
 
-# Calculate volume using (4/3(pi)r2)
-Volume = 4/3 * 3.14 * Radius**2
+# Calculate volume using (4/3(π)r**2)
+    Volume = 4/3 * 3.14 * Radius**2
 
-# Calculate surface area (4(pi)r2)
-
+# Calculate surface area (4(π)r**2)
+    SA = 4 * 3.14 * Radius**2
 
 # Print calculated volume and SA
-print(Volume)
+    print("Your volume is: ", Volume, "Your surface area is: ", SA)
