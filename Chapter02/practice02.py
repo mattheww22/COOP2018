6605a11b6a181d4c13d9ce49489aaf1bf242a9b9
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-10-01 11:27:19 -0500

    These are inital versions of Unit 2

---
+++ Chapter02/practice02.py
@@ -0,0 +1,44 @@
+# practice02.py
+#
+# Author: Matthew Wiggans
+# Course: Coding for OOP
+# Section: A3
+#     Date: 01 Oct 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 02
+#     Source: Python Programming
+#    Chapter: Practice
+#
+# Program Description
+#
+#   This Program is an interactive calculator that accepts mathematical expressions.
+#
+#
+# Algorith (pseudocode)
+#
+# Introduce program
+# Loop until exit requested
+# Get input
+# Test for exit
+# Evaluate mathematical expression
+# Print result
+#
+
+def main():
+    # Introduce program
+    print("This Program is an interactive calculator that accepts mathematical expressions.")
+    # Loop until exit requested
+    while True:
+        # Get input
+        expr = eval(input("Please enter a mathematical expression to evaluate (0 to quit): "))
+        # Test for exit
+        if expr == 0:
+            break
+        # Evaluate mathematical expression
+
+        # Print result
+        print("", expr)
+
+main()
