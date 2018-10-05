641789f0e587eb4f6b4b2c0f443e4342fc31442f
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-10-05 11:26:00 -0500

    Updated Exercise 1

--- Chapter02/U02_Ex01_Introduction.py
+++ Chapter02/U02_Ex01_Introduction.py
@@ -24,4 +24,7 @@
 #
 
 def main():
-    print("Welcome to my python file!!!")
+    print("This program converts celsius to fahrenheit!")
+    celsius = eval(input("What is the celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print ("The temperature is", fahrenheit, "degrees Fahrenheit")
