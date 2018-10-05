bb6e4d2582b72fa3ac1b2c0ffe4ba8c4b94ec858
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-10-05 10:41:44 -0500

    Exercise 1 is in!

---
+++ Chapter02/U02_Ex01_Introduction.py
@@ -0,0 +1,27 @@
+# U02_Ex01_Introduction.py
+#
+# Author: Matthew Wiggans
+# Course: Coding for OOP
+# Section: A3
+#     Date: 05 Oct 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 1
+#     Source: Python Programming
+#    Chapter: 2
+#
+# Program Description
+# This program prints an introduction to my python files
+#
+#
+#
+# Algorith (pseudocode)
+#
+#   Print introduction
+#
+#
+#
+
+def main():
+    print("Welcome to my python file!!!")
