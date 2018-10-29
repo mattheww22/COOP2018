4a2c5633e1b29c952024d82efc02e0b2decd176c
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-10-29 08:54:18 -0500

    Initial Ex 8 file

--- Chapter02/U02_Ex08_APRCompound.py
+++ Chapter02/U02_Ex08_APRCompound.py
@@ -22,3 +22,16 @@
 #
 #
 #
+def main():
+    print("This program calculates the future value")
+    print("of a 10-year investment.")
+
+    principal = eval(input("Enter principal here: "))
+    apr = eval(input("Enter the annual intrest rate:"))
+
+    for i in range(10):
+        principal = principal * (1 + apr)
+
+    print("The value in 10 years is:", principal)
+
+main()
