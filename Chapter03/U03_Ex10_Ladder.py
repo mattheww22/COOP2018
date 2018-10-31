82e72a7031b0a1ce912021537d3a3918f69c5813
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-10-31 10:42:26 -0500

    Initial Ex 10 file

---
+++ Chapter03/U03_Ex10_Ladder.py
@@ -0,0 +1,35 @@
+# U03_Ex10_Ladder.py
+#
+# Author:
+# Course: Coding for OOP
+# Section: A3
+#     Date: 31 Oct 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise:
+#     Source: Python Programming
+#    Chapter:
+#
+# Program Description
+#
+#   This program finds the length of a ladder.
+#
+#
+# Algorith (pseudocode)
+#
+#
+#
+#
+#
+
+from math import pi
+
+def main():
+
+    print("This program finds the length of a ladder.")
+    L, Deg = int(input("Enter your length and degrees of your ladder (Separated by commas): "))
+    radians = pi / 180 * Deg
+    height = L * radians
+    print("Your ladder is ", height, "tall")
+main()
