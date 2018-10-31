3da4acb068774d5482743a0c98c06aea14674a8e
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-10-31 10:00:25 -0500

    Initial Ex 4 file

---
+++ Chapter02/U02_Ex04_Convert.py
@@ -0,0 +1,34 @@
+# U02_Ex04_Convert.py
+#
+# Author: Matthew
+# Course: Coding for OOP
+# Section: A3
+#     Date: 31 Oct 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 4
+#     Source: Python Programming
+#    Chapter: 2
+#
+# Program Description
+#
+#   This
+#
+#
+# Algorith (pseudocode)
+#
+#
+#
+#
+#
+
+def main():
+
+    for i in range(5):
+
+        celsius = eval(input("What is the Celsius temperature?"))
+        farenheit = 9/5 * celsius + 32
+        print("The temperature is ", farenheit, "degrees farenheit")
+
+main()
