6f25c9537f982d1466cb5d55faafde24599ce8c1
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-11-01 13:07:19 -0500

    Initial Ex 12 file

---
+++ Chapter03/U03_Ex12_Cubes.py
@@ -0,0 +1,34 @@
+# U03_Ex12_Cubes.py
+#
+# Author:
+# Course: Coding for OOP
+# Section: A3
+#     Date: 31 Oct 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise:
+#     Source: Python Programming
+#    Chapter:
+#
+#   This program prints all of the natural numbers to 100
+#
+#
+# Algorith (pseudocode)
+#
+# Print the natural numbers
+#
+#
+#
+def main():
+    n = int(input("How many natural numbers do you want?"))
+
+    amount = 0
+
+    for i in range(n):
+        amount = amount + 1 ** 3
+
+        print(amount)
+
+
+main()
