9ae404b90f055972ded13a4d6b4b0081cb3ccf04
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-11-05 13:10:06 -0600

    Updated 10

--- Chapter03/U03_Ex10_Ladder.py
+++ Chapter03/U03_Ex10_Ladder.py
@@ -1,27 +1,27 @@
 # U03_Ex10_Ladder.py
 #
-# Author:
+# Author: Matthew Wiggans
 # Course: Coding for OOP
 # Section: A3
 #     Date: 31 Oct 2018
 #      IDE: PyCharm
 #
 # Assignment Info
-#   Exercise:
+#   Exercise: 10
 #     Source: Python Programming
-#    Chapter:
+#    Chapter: 3
 #
 # Program Description
 #
-#   This program finds the length of a ladder.
+#   This program finds the length of a ladder using the angle and the height.
 #
 #
 # Algorith (pseudocode)
 #
 #   Get length and deg from user
 #   Convert deg to radians using pi/180* deg
-#
-#
+#   Find the height using the formula (length * radians)
+#   Print the height in inches
 #
 #
 from math import pi
@@ -32,5 +32,5 @@ def main():
     L, Deg = int(input("Enter your length and degrees of your ladder (Separated by commas): "))
     radians = pi / 180 * Deg
     height = L * radians
-    print("Your ladder is ", height, "tall")
+    print("Your ladder is ", height, "inches tall")
 main()
