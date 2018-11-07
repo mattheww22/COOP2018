d19035c918d19f63979074a6027c3c65bef57c69
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-11-07 13:58:49 -0600

    updated Ex 9 file

--- Chapter03/U03_Ex09_Triangle.py
+++ Chapter03/U03_Ex09_Triangle.py
@@ -1,15 +1,15 @@
 # U03_Ex09_Triangle.py
 #
-# Author:
+# Author: Matthew Wiggans
 # Course: Coding for OOP
 # Section: A3
 #     Date: 31 Oct 2018
 #      IDE: PyCharm
 #
 # Assignment Info
-#   Exercise:
+#   Exercise: 9
 #     Source: Python Programming
-#    Chapter:
+#    Chapter: 3
 #
 # Program Description
 #
@@ -18,8 +18,8 @@
 #
 # Algorith (pseudocode)
 #
-#   Get inpup of a, b, c from user
-#   Calculate area using a+ b+ c / 2
+#   Get input of lengths a, b, c from user
+#   Calculate area using the lengths a + b + c / 2 A = s(s-a)(s - b)(s - c) // 2
 #   print area
 #
 
@@ -31,10 +31,12 @@ def main():
     s1 = s - a
     s2 = s - b
     s3 = s - c
-    A1 = s * s1 * s2 * s3
+    A1 = s * s1
+    A1 = A1 * s2
+    A1 = A1 * s3
     A = A1 // 2
 
-    print(A)
+    print(A, "is the area of your triangle")
 
 
 main()
