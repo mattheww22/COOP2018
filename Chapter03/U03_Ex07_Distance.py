89be265068782cc41f2b31b02b715ba88b7a66df
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-11-07 13:38:41 -0600

    updated Ex 7 file

--- Chapter03/U03_Ex07_Distance.py
+++ Chapter03/U03_Ex07_Distance.py
@@ -19,17 +19,18 @@
 # Algorith (pseudocode)
 #
 #   get points from user
-#   calculate distance using x2-x1**2 + y2-y1**2
+#   calculate distance using x2-x1**2 + y2-y1**2 // 2
 #   print distance
 #
 
 def main():
 
-    print("This program computes distance between two points.")
-    a, b, c, d = eval(input("What are your points?"))
-    y = d - b ** 2
-    x = c - a ** 2
+    print("This program computes distance between two points on a graph.")
+    x1, y1, x2, y2 = eval(input("What are your points?"))
+    y = y2 - y1 ** 2
+    x = x2 - x1 ** 2
     distance = x + y
+    distance = distance // 2
     print(distance, " is your distance")
 
 
