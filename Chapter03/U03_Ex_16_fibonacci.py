ea89bf79e13e8184563ed04c188e1a0857ae5e18
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-11-07 20:58:53 -0600

    updated Ex 16 file

--- Chapter03/U03_Ex_16_fibonacci.py
+++ Chapter03/U03_Ex_16_fibonacci.py
@@ -1,35 +1,46 @@
 # U03_Ex_16_fibonacci.py
 #
-# Author:
+# Author: Matthew Wiggans
 # Course: Coding for OOP
 # Section: A3
 #     Date: 29 Oct 2018
 #      IDE: PyCharm
 #
 # Assignment Info
-#   Exercise:
+#   Exercise: 16
 #     Source: Python Programming
-#    Chapter:
+#    Chapter: 3
 #
 # Program Description
-# This program computes the fibonacci of a given number
 #
-#
+# This program computes the number of fibonacci numbers defined by a user.
 #
 # Algorith (pseudocode)
 #
-#
-#
-#
+#   print program description
+#   Get input of number of sequences
+#   if terms is 1 print 1
+#   if terms = 2 print 1 1
+#   if else, compute other terms, print as you go
 #
 
 def main():
     print("This program prints the fibonacci sequence.")
     n = int(input("How many numbers do you want? "))
     a = 1
-    for i in range(n):
-        a = a + a
-        print(a)
+    b = 1
+
+    if n == 1:
+        print("1")
+
+    elif n == 2:
+        print("1 1")
+
+    for i in range(2, n):
+        c = a + b
+        print(c, end=" ")
+        a = b
+        b = c
 
 
 main()
