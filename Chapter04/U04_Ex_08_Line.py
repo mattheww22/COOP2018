fd79d67235e3544114cc2db537444f11a8dca362
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-11-08 11:05:43 -0600

    Initial Ex 8 file

---
+++ Chapter04/U04_Ex_08_Line.py
@@ -0,0 +1,24 @@
+# U04_Ex_08_Line.py
+#
+# Author: Matthew Wiggans
+# Course: Coding for OOP
+# Section: A3
+#     Date: 01 Nov 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 8
+#     Source: Python Programming
+#    Chapter: 4
+#
+# Program Description
+#
+#   This
+#
+#
+# Algorith (pseudocode)
+#
+#
+#
+#
+#
