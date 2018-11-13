1432c71e5249806dd05fd7814d05c48aae5ffe03
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2018-11-12 19:51:21 -0600

    Initial Ex 1 file

--- Chapter04/U04_Ex_05_Dice.py
+++ Chapter04/U04_Ex_05_Dice.py
@@ -26,61 +26,31 @@
 #
 
 from graphics import *
-from U04_Ex04_WinterScene import drawCircle
+
 
 def main():
-    win = GraphWin("Dice", 560, 120)
-    win.setBackground("green4")
+    win = GraphWin(1470, 270)
 
-    # draw dies 1 - 5 for straight
-    for i in range(1, 6):
-        drawDie(win, i)
-    win.getMouse()
+    win.setCoords(0, 0, 49, 9)
+    win.setBackground("black")
 
-def drawDie(win, rank):
-    """
-    Draws 1 die of given rank
-    Algorithm: 1) draw rectangle for die; 2) draw pips based on rank
-    :param win: GraphWin object
-    :param rank: int -> rank of die
-    """
-    rect = Rectangle(Point(10 + 110*(rank - 1), 10), Point(110 + 110*(rank - 1), 110))
+def makeRect(p1, p2):
+    rect = Rectangle(p1, p2)
+    rect.setFill("Forest Green")
+    rect.setOutline("Black")
+    return rect
+
+def makeDot():
+    rect = Rectangle(p1, p2)
+    rect.setFill("black")
     rect.setOutline("black")
-    rect.setFill("white")
-    rect.draw(win)
-
-    if rank % 2 == 1:
-        drawPipCenter(win, rect.getCenter())
-
-    if rank == 2 or rank == 3:
-        drawPipTL(win, rect.getCenter())
-        drawPipBR(win, rect.getCenter())
+    return rect
 
 
-    if rank == 4 or rank == 5:
-        drawPipTL(win, rect.getCenter())
-        drawPipBL(win, rect.getCenter())
-        drawPipTR(win, rect.getCenter())
-        drawPipBR(win, rect.getCenter())
+def dice1():
+    die = makeRect (Point(1, 1), Point(8, 8))
+    die.setOutline("black")
+    die.setFill("white")
 
-def drawPipCenter(win, dieCenter):
-
-    drawCircle(dieCenter, 7, "black", "black").draw(win)
-
-def drawPipTL(win, dieCenter):
-
-    drawCircle(Point(dieCenter.getX() - 30, dieCenter.getY() - 30), 7, "black", "black").draw(win)
-
-def drawPipTR(win, dieCenter):
-
-    drawCircle(Point(dieCenter.getX() + 30, dieCenter.getY() - 30), 7, "black", "black").draw(win)
-
-def drawPipBL(win, dieCenter):
-
-    drawCircle(Point(dieCenter.getX() - 30, dieCenter.getY() + 30), 7, "black", "black").draw(win)
-
-def drawPipBR(win, dieCenter):
-
-    drawCircle(Point(dieCenter.getX() + 30, dieCenter.getY() + 30), 7, "black", "black").draw(win)
 
 main()
