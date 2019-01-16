90a51d11169d903af67a307c0e7cafc9e17ef023
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-01-16 06:15:56 -0600

    Updated Excercise 9

---
+++ Chapter05/U05_Ex09_words.py
@@ -0,0 +1,38 @@
+# U05_Ex09_words.py
+#
+# Author: Matthew
+# Course: Coding for OOP
+# Section: A3
+#     Date: 06 Dec 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 5
+#     Source: Python Programming
+#    Chapter: 9
+#
+# Program Description
+#
+#   This program calculates the number of words in a sentence.
+#
+#
+# Algorith (pseudocode)
+#   Get input of sentence
+#   calculate number of words by splitting on ' '
+#   print number of words
+#
+#
+def main():
+    print('This sentence counts the number of words in a sentence.')
+    words = input('Enter your sentence: ')
+    numWord = words.split()
+    Acc = 0
+        for word in words():
+
+            for char in word()
+
+                Acc = Acc + (ord - 97)
+
+    print("The value of your string is ", Acc)
+
+main()
