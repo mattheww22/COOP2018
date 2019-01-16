fefe3beea64a2bfff83648f93779da88cbc331a1
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-01-16 06:16:50 -0600

    Excercise 8

---
+++ Chapter05/U05_Ex08_Ceasar.py
@@ -0,0 +1,29 @@
+# U05_Ex08_Ceasar.py
+#
+# Author: Matthew Wiggans
+# Course: Coding for OOP
+# Section: A3
+#     Date: 06 Dec 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 8
+#     Source: Python Programming
+#    Chapter: 5
+#
+# Program Description
+#
+#   This program will encode your sentence using a Caesar Cypher
+#
+#
+# Algorith (pseudocode)
+#
+#   Get input of word
+#   make lower case
+#   transfer into unicode
+#   print numbers
+#
+def main():
+
+    word = input('Enter word to encode: ')
+
