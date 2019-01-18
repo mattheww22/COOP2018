851a9073ce619a106f3fe5a2d9a5a0feef06544a
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-01-18 10:42:05 -0600

    recommitting EVERYTHING!

---
+++ Practices/Practice 3.py
@@ -0,0 +1,31 @@
+# Practice 3.py
+#
+# Author:
+# Course: Coding for OOP
+# Section: A3
+#     Date: 25 Oct 2018
+#      IDE: PyCharm
+#
+# Assignment Info
+#   Exercise:
+#     Source: Python Programming
+#    Chapter:
+#
+# Program Description
+#
+#
+#
+#
+# Algorith (pseudocode)
+#
+#
+#
+#
+#
+from graphics import *
+
+def main():
+
+    win = GraphWin("Fun with graphics.py")
+
+main()
