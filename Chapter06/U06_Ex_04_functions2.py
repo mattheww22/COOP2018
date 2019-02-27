2d51fef2aafc57fe735592c9bd4c96772b1129de
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-02-27 11:22:06 -0600

    Initial 4

--- Chapter06/U06_Ex_04_functions2.py
+++ Chapter06/U06_Ex_04_functions2.py
@@ -13,17 +13,48 @@
 #
 # Program Description
 #
-#   This program prints the surface area and volume of at sphere using a given radius
+#   This program returns the sum of the first "n" natural numbers and the first
 #
 # Algorith (pseudocode)
 #
-#
-#
-#
+#   get "numbers" from the user
+#   initialize n as 0
+#   initialize cubed as 0
+#   split numbers on space
+#   add the numbers to n in a loop
+#   get the cubes of all of the numbers
+#   add the cubed numbers to "cubed"
+#   print n and cubed
 #
 
 def main():
+    print("This program calculates the sum of natural numbers imputed by a user.")
+
+    n = int(input("How many natural numbers do you want to calculate? "))
+
+    calc = sum(n)
+
+    print("The sum of your natural numbers is {0}".format(calc))
+
+    sumC = sumCubed(n)
+
+    print("The sum of your natural numbers cubed is {0}".format(sumC))
 
 
+def sum(n):
+    sum = 0
+    for num in range(n):
+        sum = sum + (num + 1)
+    return sum
 
-main()
+
+def sumCubed(n):
+
+    sumC = 0
+    for num in range(n):
+        sumC = sumC + (num + 1) ** 3
+    return sumC
+
+
+if __name__ == '__main__':
+    main()
