29d9e0b43ba6ccac6295521ce79af27dc751df45
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-02-28 15:11:53 -0600

    Updated 4

--- Chapter05/U05_Ex04_Acronym.py
+++ Chapter05/U05_Ex04_Acronym.py
@@ -1,39 +1,48 @@
 # U05_Ex04_Acronym.py
 #
-# Author:
+# Author: Matthew Wiggans
 # Course: Coding for OOP
 # Section: A3
 #     Date: 30 Nov 2018
 #      IDE: PyCharm
 #
 # Assignment Info
-#   Exercise:
+#   Exercise: 04
 #     Source: Python Programming
-#    Chapter:
+#    Chapter: 05
 #
 # Program Description
 #
-#   This program makes an acronym out of any string of words you want
+#   This program creates an acronym out of a string of words given by the user.
 #
 #
 # Algorith (pseudocode)
 #
-#   Get string inputs from user
-#   Get the first letter of every word
-#   print all acronym letters capitalized in a string
+#   Print intro
+#   Get phrase from the user
+#   Create list of words by splitting on space
+#   Send to func
+#       Get the first letter of every word and capitalize it
+#       Return Acronym
+#   Print Acronym and initial phrase
 #
 
 def main():
     print("This program makes an acronym out of any string of words you want")
 
-    words = input("What are the words you want to be turned into acronyms? ").split()
+    phrase = input("What are the words you want to be turned into acronyms? ")
 
+    words = phrase.split(" ")
+    acronym = Acronym(words)
+
+    print("{0} is the acronym for {1}".format(acronym, phrase))
+
+def Acronym(words):
     acronym = ''
-
     for word in words:
 
         acronym += word[0].upper()
-        print(acronym, "is the acronym for ", words)
 
+    return acronym
 
 main()
