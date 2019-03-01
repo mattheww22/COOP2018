5f5d52a3bb7348b8d1da564fdb2868d59bd9d1a9
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-03-01 01:03:04 -0600

    Initial 16

---
+++ Chapter07/U07_Ex16_Archery.py
@@ -0,0 +1,23 @@
+"""
+U07_Ex16_Archery.py
+
+  Author: Matthew Wiggans
+  Course: Coding for OOP
+ Section: A2
+    Date: 2019-02-27
+     IDE: PyCharm
+
+Assignment Info
+  Exercise: 16
+    Source: Python Programming
+   Chapter: 07
+
+Program Description
+
+    This program draws a target that you can shoot at for a score!
+
+Algorithm
+
+
+
+"""
