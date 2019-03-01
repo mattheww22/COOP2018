541f8536715d94b6b4c2fc2402a885a317f26266
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-03-01 01:02:54 -0600

    Initial 17

---
+++ Chapter07/U07_Ex17_Circle.py
@@ -0,0 +1,23 @@
+"""
+U07_Ex17_Circle.py
+
+  Author: Matthew Wiggans
+  Course: Coding for OOP
+ Section: A2
+    Date: 2019-02-27
+     IDE: PyCharm
+
+Assignment Info
+  Exercise: 17
+    Source: Python Programming
+   Chapter: 07
+
+Program Description
+
+    This program draws a ball bouncing around a box on the screen.
+
+Algorithm
+
+
+
+"""
