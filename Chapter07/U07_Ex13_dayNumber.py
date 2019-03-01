e7b639b92ed0e216ee82819bb1d9e2bb44849006
Matthew Wiggans <matthew.w22@student.parishepiscopal.org>
2019-03-01 01:03:17 -0600

    Initial 13

--- Chapter07/U07_Ex13_dayNumber.py
+++ Chapter07/U07_Ex13_dayNumber.py
@@ -1,9 +1,9 @@
 """
 U07_Ex13_dayNumber.py
 
-  Author: 
+  Author: Matthew Wiggans
   Course: Coding for OOP
- Section: 
+ Section: A2
     Date: 2019-02-21
      IDE: PyCharm
 
@@ -14,13 +14,17 @@ Assignment Info
 
 Program Description
 
+    This program calculates the
 
 Algorithm
 
-
-
+    Get the input of the date from the user
+    Send to func
+    Initialize string of days(Jan 1 - Dec 31)
+    Parse string by finding the date number in list (Ex. Jan 4: Looks for 4th Jan)
+    Calculate the
 """
-def main()
+def main():
 
     print("This program will show what day of the year that date is.")
     date = input("What date do you want to calculate?(Ex; Jan 24) ")
